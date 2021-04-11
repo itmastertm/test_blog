7c8af05ff7c0508c5c04ef26a47733d074e0c35e
Vitaliy Vinnikov <itmastertm@gmail.com>
2021-04-12 02:46:16 +0500

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -21,11 +22,9 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
 		if @post.update_attributes(post_params)
 			redirect_to @post
 		else
@@ -33,7 +32,16 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
 	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
 
 	def post_params
 		params.require(:post).permit(:title, :summary, :body)
